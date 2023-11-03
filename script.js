738f52e70751c35ab52d7ddb11f2b9915ab158ca
Akshithadogga <117561482+Akshithadogga@users.noreply.github.com>
2023-11-04 02:08:08 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,23 @@
 function threeSum(arr, target) {
 // write your code here
+	var sum;
+	for(let i=0;i<arr.length-2;i++){
+		for(let j=0;j<arr.length-1;j++){
+			for(let k=0;k<arr.length;k++){
+				sum=arr[i]+arr[j]+arr[k];
+			}
+		}
+	}
+	let arr;
+	var ans;
+for(let i=0;i<sum.length-1;i++){
+	arr=Math.abs(sum[i]-target);
+	ans=Math.min(arr[i],arr[i+1]);
+	if(ans==arr[i]){
+		return sum[i];
+	}
+}
+	// return ans;
   
 }
 
