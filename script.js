52eaa1995052e41cfc7c9a3cd5f98b38b0024db1
Akshithadogga <117561482+Akshithadogga@users.noreply.github.com>
2023-11-04 11:38:23 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,24 +1,33 @@
 function threeSum(arr, target) {
 // write your code here
-	var sum;
-	for(let i=0;i<arr.length-2;i++){
-		for(let j=0;j<arr.length-1;j++){
-			for(let k=0;k<arr.length;k++){
-				sum=arr[i]+arr[j]+arr[k];
+  var sum=undefined;
+		var ans;
+		let least=Infinity;
+		for(let i=0;i<arr.length-2;i++){
+			for(let j=i+1;j<arr.length-1;j++){
+				for(let k=j+1;k<arr.length;k++){
+					if(sum!=undefined){
+						let presentsum=arr[i]+arr[j]+arr[k];
+						let premin=sum-target;
+						let present=presentsum-target;
+						let minval=Math.min(Math.abs(premin,present));
+					
+						if(minval<least){
+							least=minval;
+								if(minval==premin){
+							ans=sum;
+						}
+						else{
+							ans=presentsum;
+						}
+						}
+						
+						
+					}
+					sum=arr[i]+arr[j]+arr[k];
+				}
 			}
 		}
-	}
-	let arr;
-	var ans;
-for(let i=0;i<sum.length-1;i++){
-	arr=Math.abs(sum[i]-target);
-	ans=Math.min(arr[i],arr[i+1]);
-	if(ans==arr[i]){
-		return sum[i];
-	}
+		return ans;
 }
-	// return ans;
-  
-}
-
 module.exports = threeSum;
